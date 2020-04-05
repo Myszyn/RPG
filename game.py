443b5d16206657f505c11c4373570c43e0ab53d9
Myszyn <61430264+Myszyn@users.noreply.github.com>
2020-04-05 14:15:36 +0200

    Added members to our team. Added hp/mp bars.
    You can fight with your team now. Every turn you select their actions.

--- game.py
+++ game.py
@@ -14,7 +14,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 class Person:
-    def __init__(self,atc, hp, mp, df, magic, dodge,armor,items):
+    def __init__(self,name,atc, hp, mp, df, magic, dodge,armor,items):
         self.atc = atc
         self.atkl = atc-10
         self.atkh =  atc+10
@@ -28,6 +28,7 @@ class Person:
         self.actions = ["Attack ","Magic","Items"]
         self.dodge = dodge
         self.items = items
+        self.name = name
 
     def generate_dmg(self):
         return random.randrange(self.atkl,self.atkh)
@@ -57,6 +58,9 @@ class Person:
 
     def get_hp(self):
         return self.hp
+
+    def get_name(self):
+        return self.name
 
     def get_mp(self):
         return self.mp
@@ -95,6 +99,83 @@ class Person:
         for items in self.items:
             print(str(i),":",items.name,"  :",str(items.description),"(x",items.quantity,")")
             i+=1
-#=========================================================================================================
+#==========================================================================================================
     def generate_information(self):
-        print(self.hp , " life points","/",self.atc,"attack points")
+        print(self.name, self.hp , " life points","/",self.atc,"attack points")
+    def get_stats(self):
+        hp_bar  = ""
+        mp_bar  = ""
+        hp_bar_ticks = (self.hp/self.mhp)*100/4
+        mp_bar_ticks = (self.mp/self.mmp)*100/10
+        while hp_bar_ticks>0:
+            hp_bar+="█"
+            hp_bar_ticks -=1
+        while len(hp_bar)<25:
+            hp_bar+=" "
+        while mp_bar_ticks>0:
+            mp_bar+="█"
+            mp_bar_ticks -=1
+        while len(mp_bar)<10:
+            mp_bar+=" "
+
+        hp_string= str(self.hp)+"/"+str(self.mhp)
+        mp_string= str(self.mp)+"/"+str(self.mmp)
+        current_hp = ''
+        current_mp = ''
+        if len(hp_string)<9:
+            decreased = 9-len(hp_string)
+            while decreased>0:
+                hp_string+=" "
+                decreased-=1
+            #hp_string+=current_hp
+
+
+        if len(hp_string)<9:
+            decreased = 9-len(mp_string)
+            while decreased>0:
+                mp_string+=" "
+                decreased-=1
+            #mp_string+=current_mp
+
+
+        print(self.name,":     ",hp_string,"[",hp_bar,"]  ", mp_string," [",mp_bar,"]","\n")
+
+
+    def endturn_stats(self):
+        print(self.name,"health points :", self.hp ,"/",self.mhp,"mana points :",self.mp,"/",self.mmp)
+
+    def generate_information(self):
+        print(self.name, self.hp , " life points","/",self.atc,"attack points")
+    def get_enemy_stats(self):
+        hp_bar  = ""
+        mp_bar  = ""
+        hp_bar_ticks = (self.hp/self.mhp)*100/4
+        while hp_bar_ticks>0:
+            hp_bar+="█"
+            hp_bar_ticks -=1
+        while len(hp_bar)<25:
+            hp_bar+=" "
+
+        hp_string= str(self.hp)+"/"+str(self.mhp)
+        current_hp = ''
+        if len(hp_string)<11:
+            decreased = 11-len(hp_string)
+            while decreased>0:
+                hp_string+=" "
+                decreased-=1
+            #hp_string+=current_hp
+
+
+        print(self.name,":     ",hp_string,"[",hp_bar,"]  ")
+
+
+
+
+
+
+
+        
+            
+
+
+
