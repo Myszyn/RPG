2b5dea3df4b997d72c5df9e16d7817f0c62e4a05
Myszyn <61430264+Myszyn@users.noreply.github.com>
2020-04-05 02:09:34 +0200

    Added new parameter. New funtions

--- magic.py
+++ magic.py
@@ -1,15 +1,20 @@
 import random
 
 class Spell:
-    def __init__(self,name,cost,dmg,type):
+    def __init__(self,name,cost,dmg,type,turn):
         self.name=name
         self.cost = cost
         self.dmg = dmg
         self.hdmg  = dmg + 20
         self.ldmg = dmg - 20
         self.type = type
+        self.turn = turn
     def generate_damage(self):
         return random.randrange(self.ldmg,self.hdmg)
+    def get_spell_dmg(self):
+        return self.dmg
+    def get_spell_turn(self):
+        return self.turnself.hdmg)
 
 
     
