147b128eb69dc0b45059ecdb84c75e3e60c779af
Myszyn <61430264+Myszyn@users.noreply.github.com>
2020-04-08 20:43:58 +0200

    Deleted some "armor" methods and added to game.py

--- item.py
+++ item.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Armor:
     def __init__(self,name,atc,hp,dodge,df,mp):
         self.atc = atc
@@ -9,19 +10,9 @@ class Armor:
         self.mp = mp
         self.name = name
 
-    def choose_armor(self):
-        print("Choose armor")
-        for item in self.armor:
-            print(str(i),":",spell.name,"cost:",str(spell.cost),"damage:",str(spell.dmg))
-            i+=1 
+ 
 
-    def give_armor(self):
-        player.atc += self.atc
-        player.hp += self.hp
-        player.dodge += self.dodge
-        player.df += self.df
-        player.mp += self.mp
-
+   
 class Items:
     def __init__(self,name,type,description,prop,quantity):
         self.type= type
